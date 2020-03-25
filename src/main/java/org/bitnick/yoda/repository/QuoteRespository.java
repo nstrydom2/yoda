c79915c7febfef5ab38965f2bf63f97e6c4c289c
Nick <nstrydom@gmail.com>
2020-03-25 14:51:24 -0500

    Added swagger

--- src/main/java/org/bitnick/yoda/repository/QuoteRespository.java
+++ src/main/java/org/bitnick/yoda/repository/QuoteRespository.java
@@ -2,8 +2,11 @@ package org.bitnick.yoda.repository;
 
 import org.bitnick.yoda.model.Quote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuoteRespository extends JpaRepository<Quote, String> {
+    @Query("SELECT q FROM yoda_quotes q ORDER BY random() LIMIT 1")
+    Quote findOneRandom();
 }
--- src/main/java/org/bitnick/yoda/service/YodaQuoteService.java
+++ src/main/java/org/bitnick/yoda/service/YodaQuoteService.java
@@ -1,4 +1,20 @@
 package org.bitnick.yoda.service;
 
+import org.bitnick.yoda.model.Quote;
+import org.bitnick.yoda.repository.QuoteRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class YodaQuoteService {
+    private QuoteRespository quoteRespository;
+
+    @Autowired
+    public void setQuoteRespository(QuoteRespository quoteRespository) {
+        this.quoteRespository = quoteRespository;
+    }
+
+    public Quote getRandomQuote() {
+        return quoteRespository.findOneRandom();
+    }
 }
