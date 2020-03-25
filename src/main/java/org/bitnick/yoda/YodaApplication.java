2b77123cb25f6c0cc0313781d127db004d62a1a9
Nick <nstrydom@gmail.com>
2020-03-25 16:17:19 -0500

    Hotfix YodaQuoteService

--- src/main/java/org/bitnick/yoda/YodaApplication.java
+++ src/main/java/org/bitnick/yoda/YodaApplication.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class YodaApplication implements CommandLineRunner {
 	private YodaScraper yodaScraper;
 
