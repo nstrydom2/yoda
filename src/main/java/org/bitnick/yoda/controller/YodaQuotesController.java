47c4a4b58a93c0db93a2315f61cc1c918d9b75cb
Nick <nstrydom@gmail.com>
2020-03-25 14:57:07 -0500

    Implemented YodaQuotesController.java

--- src/main/java/org/bitnick/yoda/controller/YodaQuotesController.java
+++ src/main/java/org/bitnick/yoda/controller/YodaQuotesController.java
@@ -1,7 +1,23 @@
 package org.bitnick.yoda.controller;
 
+import org.bitnick.yoda.model.Quote;
+import org.bitnick.yoda.service.YodaQuoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class YodaQuotesController {
+    private YodaQuoteService yodaQuoteService;
+
+    @Autowired
+    public void setYodaQuoteService(YodaQuoteService yodaQuoteService) {
+        this.yodaQuoteService = yodaQuoteService;
+    }
+
+    @RequestMapping(path = "yoda/", method = RequestMethod.GET)
+    public Quote getRandomQuote() {
+        return yodaQuoteService.getRandomQuote();
+    }
 }
